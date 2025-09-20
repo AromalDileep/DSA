2acf51cef8dd572afc3da1228400021e63d7de9e
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-09-20 17:20:45 +0530

    Time: 8417 ms (5.00%) | Memory: 17.7 MB (96.13%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest_pal = "" 
+
+        for i in range(len(s)):
+            l = i
+            r = len(s) - 1
+
+            while l <= r:  
+                temp = s[l:r + 1]  
+                if temp == temp[::-1]:  
+                   
+                    if len(temp) > len(longest_pal):
+                        longest_pal = temp
+                r -= 1
+
+        return longest_pal 
