0794b2aa030ef8ce261004d99e306ab1a5acde71
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-09-22 19:21:37 +0530

    Time: 42 ms (43.25%) | Memory: 17.8 MB (60.42%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -0,0 +1,25 @@
+import re
+class Codec:
+
+    def __init__(self):
+        self.mp = {} #longUrl, shortUrl
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        parts = re.split(r'(/)',longUrl)
+        st = "".join(chr(random.randint(48,122)) for _ in range(6))
+        short_url = "".join(parts[:6]) + st
+        self.mp[short_url] = longUrl
+        return short_url
+        
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        return self.mp[shortUrl]
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
