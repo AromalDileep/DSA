9dfa0a3bed76e42ab5c7c5937d3ec55b80925c1d
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-09-27 15:44:47 +0530

    Time: 421 ms (73.24%) | Memory: 18.1 MB (12.99%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -2,18 +2,28 @@ from typing import List
 
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        """
+        Count the number of triplets that can form valid triangles.
+        A triangle is valid if nums[i] + nums[j] > nums[k],
+        where nums[k] is chosen as the largest side.
+        """
         nums.sort()
         n = len(nums)
         count = 0
-        
-        # Fix the largest side nums[k]
-        for k in range(n - 1, 1, -1):
-            i, j = 0, k - 1
-            while i < j:
-                if nums[i] + nums[j] > nums[k]:
-                    count += j - i
-                    j -= 1
+
+        # Fix the largest side nums[largest]
+        for largest in range(n - 1, 1, -1):
+            left, right = 0, largest - 1
+
+            while left < right:
+                if nums[left] + nums[right] > nums[largest]:
+                    # If nums[left] + nums[right] > nums[largest],
+                    # then all elements between left..(right-1) with nums[right]
+                    # also satisfy the condition because the array is sorted.
+                    count += right - left
+                    right -= 1
                 else:
-                    i += 1
-        
+                    # Otherwise, increase left to make the sum larger
+                    left += 1
+
         return count
