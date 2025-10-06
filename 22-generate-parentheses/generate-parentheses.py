47f1ec83268dbf359d588ad2239b07866d2dfc1e
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-10-06 11:41:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (43.50%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,20 +1,23 @@
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+    def generateParenthesis(self, n: int) -> list[str]:
         res = []
 
-        def backtrack(curr, open_count, close_count):
-            # base case: if length == 2 * n, add to result
-            if len(curr) == 2 * n:
-                res.append(curr)
+        def backtrack(path, open_count, close_count):
+            if len(path) == 2 * n:
+                res.append("".join(path))
                 return
 
-            # condition 1: we can add '(' if open_count < n
+            # Add '(' if we still have some left
             if open_count < n:
-                backtrack(curr + "(", open_count + 1, close_count)
+                path.append("(")               # add '('
+                backtrack(path, open_count + 1, close_count)
+                path.pop()                     # remove '(' after recursion
 
-            # condition 2: we can add ')' if close_count < open_count
+            # Add ')' if it won't exceed the number of '('
             if close_count < open_count:
-                backtrack(curr + ")", open_count, close_count + 1)
+                path.append(")")               # add ')'
+                backtrack(path, open_count, close_count + 1)
+                path.pop()                     # remove ')' after recursion
 
-        backtrack("", 0, 0)
+        backtrack([], 0, 0)
         return res
