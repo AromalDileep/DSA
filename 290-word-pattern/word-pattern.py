3f26a5f2daf40e2bb4e4e139bc957101c54b622f
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-10-21 18:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.05%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,21 +1,7 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        hm = {}
         words = s.split()
-
-        #  Important length check
-        if len(pattern) != len(words):
-            return False
-
-        for i in range(len(pattern)):
-            if pattern[i] not in hm:
-                # prevent two letters mapping to the same word
-                if words[i] in hm.values():
-                    return False
-                hm[pattern[i]] = words[i]
-            else:
-                # check if existing mapping matches
-                if words[i] != hm[pattern[i]]:
-                    return False
-
-        return True
+        return (
+            len(words) == len(pattern)
+            and len(set(zip(pattern, words))) == len(set(pattern)) == len(set(words))
+        )
