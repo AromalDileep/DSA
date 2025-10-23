cfc1360b15b2b1af9f7960a30b04da4444a3cbff
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-10-23 17:05:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (42.97%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,11 +1,3 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        res = []
-        for i in range(1, n+1):
-            s = ''
-            if i % 3 == 0: 
-                s += 'Fizz'
-            if i % 5 == 0:
-                s += 'Buzz'
-            res.append(s or str(i))
-        return res
+        return ['Fizz'*(i%3==0) + 'Buzz'*(i%5==0) or str(i) for i in range(1, n+1)]
