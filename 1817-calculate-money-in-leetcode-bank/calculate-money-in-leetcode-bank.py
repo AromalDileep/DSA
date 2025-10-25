1bd4c84d0b83497cbde1f78987b9193f783fa666
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-10-25 12:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.22%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,15 +1,8 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        if n == 1:
-            return 1
-        monday = 0
-        res = []
-        for i in range(1, n+1): 
-            if i% 7 == 0: 
-                res.append(7 + monday)
-                monday += 1
-            else:
-                res.append(i % 7 + monday)
-        print(res)
-        return sum(res)
-        
+        weeks = n // 7
+        days = n % 7
+
+        full_weeks = 28 * weeks + 7 * (weeks * (weeks - 1)) // 2
+        remaining_days = days * (weeks + 1) + (days * (days - 1)) // 2
+        return full_weeks + remaining_days
