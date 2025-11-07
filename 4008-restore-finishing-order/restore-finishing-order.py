73a6d678d843ce3824afad9a7cefc01665dc711b
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-11-07 17:45:05 +0530

    Time: 2 ms (40.13%) | Memory: 18 MB (21.61%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.py
+++ 4008-restore-finishing-order/restore-finishing-order.py
@@ -1,3 +1,7 @@
 class Solution:
     def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
-        return [num for num in order if num in friends]
+        pos_idx = {}
+        for idx, pos in enumerate(order):
+            pos_idx[pos] = idx
+        
+        return sorted(friends, key=lambda x:pos_idx[x]) 
