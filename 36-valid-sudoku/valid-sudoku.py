2df2db424605e215e90efa9f8d58d2ed298947dc
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-02 14:03:03 +0530

    Time: 7 ms (36.16%) | Memory: 17.5 MB (99.05%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,36 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
   
-        for i in range(9):
-            seen = set()
-            for j in range(9):
-                if board[i][j] == '.':
-                    continue
-                if board[i][j] in seen:
-                    return False
-                seen.add(board[i][j])
-        
-    
-        for j in range(9):
-            seen = set()
-            for i in range(9):
-                if board[i][j] == '.':
-                    continue
-                if board[i][j] in seen:
-                    return False
-                seen.add(board[i][j])
-        
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set) # key = (r//3, c//3)
 
-        for box_row in range(0, 9, 3):
-            for box_col in range(0, 9, 3):
-                seen = set()
-                for i in range(3):
-                    for j in range(3):
-                        val = board[box_row + i][box_col + j]
-                        if val == '.':
-                            continue
-                        if val in seen:
-                            return False
-                        seen.add(val)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r]or 
+                    board[r][c] in cols[c] or 
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
         
         return True
