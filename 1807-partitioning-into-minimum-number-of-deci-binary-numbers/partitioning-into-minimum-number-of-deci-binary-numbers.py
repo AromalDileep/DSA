602ab4cb9b5191bb66b897fc7cf569c5e85aad08
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-06 17:54:57 +0530

    Time: 165 ms (10.84%) | Memory: 17.5 MB (100.00%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -4,5 +4,7 @@ class Solution:
         max_num = 0 
         for num in n:
             max_num = max(max_num, int(num))
+            if max_num == 9:
+                break
         
         return max_num
