ec2fc2648f856a1b944babb7145dd3944f623e73
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-07 16:30:07 +0530

    Time: 38 ms (50.17%) | Memory: 17.9 MB (29.92%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.py
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.py
@@ -1,7 +1,4 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        if low % 2 != 0 or high % 2 != 0: 
-            return ((high+2) - low) // 2
+        return (high + 1) // 2 - low // 2
 
-        
-        return (high - low) //2
