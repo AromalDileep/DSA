510a663debf18e87ddcbcf8ddc254a1c2531f69c
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-08 16:18:31 +0530

    Time: 160 ms (56.25%) | Memory: 17.6 MB (94.57%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -5,8 +5,8 @@ class Solution:
         for i in range(1, n + 1):
             for j in range(i, n + 1):
 
-                k = int(sqrt(i**2 + j**2))
-                if k <= n and i**2 + j**2 == k**2:
+                k = int((i*i + j*j)**0.5)
+                if k <= n and i*i +j*j == k*k:
                     count += 2
-                
+        
         return count
