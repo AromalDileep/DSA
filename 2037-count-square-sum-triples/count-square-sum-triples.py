778909addbccda96afa32a6a53e1cfec893a41c9
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-08 10:17:16 +0530

    Time: 184 ms (52.17%) | Memory: 18 MB (11.68%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -2,10 +2,11 @@ class Solution:
     def countTriples(self, n: int) -> int:
         count = 0
         
-        for a in range(1, n + 1):
-            for b in range(a, n + 1):
-                for c in range(b, n + 1):
-                    if a * a + b * b == c * c:
-                        count += 2
-        
+        for i in range(1, n + 1):
+            for j in range(i, n + 1):
+
+                k = int(sqrt(i**2 + j**2))
+                if k <= n and i**2 + j**2 == k**2:
+                    count += 2
+                
         return count
