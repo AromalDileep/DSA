794ea74696d7abaabe9808e95a9f0f141ea19221
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-11 23:17:36 +0530

    Time: 107 ms (39.30%) | Memory: 28.4 MB (90.34%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -6,14 +6,11 @@ class Solution:
         max_area = 0
         
         while left < right:
-            # Calculate the current area
             width = right - left
             current_area = min(height[left], height[right]) * width
             
-            # Update max_area if current area is larger
             max_area = max(max_area, current_area)
             
-            # Move the pointer pointing to the shorter line inward
             if height[left] < height[right]:
                 left += 1
             else:
