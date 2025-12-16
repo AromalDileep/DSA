ac8ea7b8494615b250f6ead869ca6230af14da04
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-16 16:57:53 +0530

    Time: 4 ms (79.48%) | Memory: 17.7 MB (63.11%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,7 +4,5 @@ class Solution:
         str_x = str(x)
         if len(str_x)==1:
             return True
-        for i in range(int(len(str_x)/2)):
-            if str_x[i] != str_x[len(str_x) - 1 - i]:
-                return False
-        return True
+        
+        return str_x == str_x[::-1]
