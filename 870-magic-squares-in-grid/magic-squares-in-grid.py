df7bae135d7aac333ceabf128345bb1178738682
Aromal <154106565+AromalDileep@users.noreply.github.com>
2025-12-30 21:48:47 +0530

    Time: 4 ms (12.12%) | Memory: 17.3 MB (97.58%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.py
@@ -0,0 +1,43 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        for i in range(rows - 2):
+            for j in range(cols - 2):
+                if self.is_magic(grid, i, j):
+                    count += 1
+
+        return count
+
+    def is_magic(self, grid, r, c):
+        nums = set()
+
+        # collect 3x3 numbers
+        for i in range(r, r + 3):
+            for j in range(c, c + 3):
+                val = grid[i][j]
+                if val < 1 or val > 9:
+                    return False
+                nums.add(val)
+
+        if len(nums) != 9:
+            return False
+
+        # rows
+        for i in range(3):
+            if sum(grid[r + i][c:c + 3]) != 15:
+                return False
+
+        # columns
+        for j in range(3):
+            if sum(grid[r + i][c + j] for i in range(3)) != 15:
+                return False
+
+        # diagonals
+        if grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != 15:
+            return False
+        if grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != 15:
+            return False
+
+        return True
