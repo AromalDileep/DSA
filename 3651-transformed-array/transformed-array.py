e2402f116aa3b029ee2155185aa6a1d5966efa88
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-02-05 11:14:13 +0530

    Time: 67 ms (18.83%) | Memory: 19.3 MB (34.53%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -1,10 +1,4 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        result = [0] * n
-    
-        for i, num in enumerate(nums):
-            result[i] = nums[(i + num) % n]
-        
-        return result
-
+            n = len(nums)
+            return [nums[(i + nums[i]) % n] for i in range(n)]
