9784eded7ede3ff367f997e4affed798f77913c7
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-02-09 19:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.93%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,9 +1,7 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        # Step 1: Transpose the matrix using zip
-        matrix[:] = [list(row) for row in zip(*matrix)]  # In-place update using slicing
+        matrix[:] = [list(row) for row in zip(*matrix)] 
 
-        # Step 2: Reverse each row in place
         for row in matrix:
             row.reverse()
             
