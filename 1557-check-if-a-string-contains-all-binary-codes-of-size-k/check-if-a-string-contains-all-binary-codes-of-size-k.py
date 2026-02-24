5dc847eb17fc302de7fe2c1bdcd7a7760c9610fb
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-02-24 12:44:49 +0530

    Time: 251 ms (65.54%) | Memory: 51.9 MB (52.48%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -4,8 +4,7 @@ class Solution:
             return False
         
         seen = set()
-        
-        for i in range(len(s) - k + 1):
+        for i in range(len(s)-k+1):
             seen.add(s[i:i+k])
-        
+         
         return len(seen) == 2**k
