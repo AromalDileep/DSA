8d2a9b91f2a9d95d5ad7b7f38b0450d9ea612066
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-02-27 12:18:32 +0530

    Time: 3 ms (74.24%) | Memory: 20 MB (84.94%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,10 +1,10 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         row = len(triangle)
-        memo = triangle[row-1].copy()
-
+        mem = triangle[row-1].copy()
+      
         for r in range(row-2, -1, -1):
             for c in range(r+1):
-                memo[c] = min(memo[c], memo[c+1]) + triangle[r][c]
+                mem[c] = min(mem[c], mem[c+1]) + triangle[r][c]
         
-        return memo[0]
+        return mem[0]
