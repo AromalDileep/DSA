4c676abb0f81374febb5dbca88a05b6641b5e25c
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-03-03 11:01:34 +0530

    Time: 986 ms (5.07%) | Memory: 25.5 MB (23.77%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,10 +1,11 @@
 # precomute since n <= 20 
-s = "0"
-for i in range(19):
-    inv = ""
-    for c in s: inv += "1" if c == "0" else "0"
-    s += "1" + inv[::-1]
 
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
+        s = "0"
+
+        for i in range(n):
+            inv = ""
+            for c in s: inv += "1" if c == "0" else "0"
+            s += "1" + inv[::-1]
         return s[k-1]
