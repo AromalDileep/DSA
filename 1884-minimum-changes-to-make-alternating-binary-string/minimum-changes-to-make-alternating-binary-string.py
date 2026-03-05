d50f676396fedf96560da8bfcaf6cfe607c7ca1e
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-03-05 13:43:20 +0530

    Time: 19 ms (9.76%) | Memory: 19.4 MB (56.18%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,14 +1,14 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        flip1 = 0
-        flip2 = 0 
+        flip1 = 0  # 010101
+        flip2 = 0  # 101010
 
         for i in range(len(s)):
 
             if s[i] != str(i % 2):
                 flip1 += 1
 
-            if s[i] != str((i+1) % 2):
-                flip2 += 1 
+            if s[i] != str((i + 1) % 2):
+                flip2 += 1
 
-        return min(flip1, flip2) 
+        return min(flip1, flip2)
