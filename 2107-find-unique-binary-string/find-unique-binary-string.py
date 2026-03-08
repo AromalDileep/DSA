fc750f3b0ba40ba22f45bbc76c9994958082025a
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-03-08 13:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.94%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,12 +1,11 @@
 from itertools import product
+
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         n = len(nums)
-        bins = ["0", "1"]
+        nums = set(nums)
 
-        combos = product(bins, repeat=n)
-        combos = list(map("".join, combos))
-
-        for num in combos:
-            if num not in nums:
-                return num
+        for combo in product("01", repeat=n):
+            s = "".join(combo)
+            if s not in nums:
+                return s
