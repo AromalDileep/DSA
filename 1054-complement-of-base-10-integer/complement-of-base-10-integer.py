03e4d75f2d618199cedb7ce00b61049ecbead8b9
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-03-11 17:17:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.30%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.py
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.py
@@ -2,8 +2,7 @@ class Solution:
     def bitwiseComplement(self, n: int) -> int:
         
         res = ""
-        b = bin(n)[2:]
-        for num in b:
+        for num in bin(n)[2:]:
             res += "1" if num == "0" else "0"
         
         return int(res, 2)
