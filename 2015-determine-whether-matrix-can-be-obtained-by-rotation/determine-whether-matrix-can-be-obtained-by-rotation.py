a05743f39ba80cd53f09a21466ecdc3ac24e29a4
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-03-22 12:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.41%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -7,19 +7,17 @@ class Solution:
 # swap top and bottom then swap diagonally for rotatin 90 degre
 
             # swapping top and bottom
-            for i in range(n // 2): 
-                for j in range(n):
-                    mat[i][j], mat[n-1-i][j] = mat[n-1-i][j], mat[i][j]
+            # for i in range(n // 2): 
+            #     for j in range(n):
+            #         mat[i][j], mat[n-1-i][j] = mat[n-1-i][j], mat[i][j]
             # or
-            # mat.reverse()
+            mat.reverse()
 
             # swapping diagonals (works for square matrix)
 
             for i in range(n):
                 for j in range(i + 1):
                     mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
-            # or 
-            # mat= [list(row) for row in zip(*mat)]
             return mat == target
         
         for i in range(4):
