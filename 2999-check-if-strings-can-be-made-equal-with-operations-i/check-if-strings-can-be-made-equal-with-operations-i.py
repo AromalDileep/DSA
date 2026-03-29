179485c5f46ace03d787313143716b135603a445
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-03-29 12:17:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.85%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,9 +1,10 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
         
-        a, b, c, d = s2
-
-        return s1 in (s2, c+b+a+d, a+d+c+b, c+d+a+b)
+        return (
+            sorted([s1[0], s1[2]]) == sorted([s2[2], s2[0]]) and
+            sorted([s1[1], s1[3]]) == sorted([s2[3], s2[1]])
+        )
 
             
                 
