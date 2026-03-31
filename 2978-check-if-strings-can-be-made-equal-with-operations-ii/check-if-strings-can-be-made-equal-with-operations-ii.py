a966ddb8417091c222013534a9d6bc3146daa06f
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-03-31 16:33:22 +0530

    Time: 47 ms (90.62%) | Memory: 20.1 MB (77.05%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -2,6 +2,6 @@ class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
         
         return (
-            sorted(s1[::2]) == sorted(s2[::2]) and
-            sorted(s1[1::2]) == sorted(s2[1::2])
+            Counter(s1[::2]) == Counter(s2[::2]) and
+            Counter(s1[1::2]) == Counter(s2[1::2])
         )
