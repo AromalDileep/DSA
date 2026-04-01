decf51edaf6c1b190c838f7cb71adb05983ae4b5
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-04-01 09:49:27 +0530

    Time: 121 ms (70.24%) | Memory: 45 MB (61.31%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -1,33 +1,38 @@
 class Solution:
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
         
-        robots = sorted([(positions[i], healths[i], directions[i], i)
-                        for i in range(len(positions))])
+        robots = sorted([(positions[i], healths[i], directions[i], i) 
+                         for i in range(len(positions))])
         
-        stack = [] # storing right index
-        health = healths[:] # copy 
-
+        stack = []  # indices of robots moving right
+        health = healths[:]  # copy
+        
         for pos, h, d, i in robots:
             if d == "R":
                 stack.append(i)
             else:
+                # fight with previous right-moving robots
                 while stack and health[i] > 0:
                     j = stack[-1]
-                    if health[j] > health[i]:
-                        health[j] -= 1
-                        health[i] = 0 
-                    elif health[j] < health[i]:
-                        health[j] = 0 
-                        health[i] -= 1
+                    
+                    if health[j] < health[i]:
                         stack.pop()
+                        health[i] -= 1
+                        health[j] = 0
+                    elif health[j] > health[i]:
+                        health[j] -= 1
+                        health[i] = 0
                     else:
                         health[j] = 0
-                        health[i] = 0 
+                        health[i] = 0
                         stack.pop()
+                        break
         
-        res = []
-        for val in health:
-            if val > 0:
-                res.append(val)
-        return res
+        # collect survivors
+        result = []
+        for i in range(len(health)):
+            if health[i] > 0:
+                result.append(health[i])
+        
+        return result
 
