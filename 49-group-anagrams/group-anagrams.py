7fe949f684e26c2ad16e7dd9c1acfa3065e16c79
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-04-03 13:55:08 +0530

    Time: 11 ms (84.67%) | Memory: 24 MB (22.90%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
         out = {}
 
         for st in strs:
             count = [0] * 26
             for c in st:
                 count[ord(c) - ord('a')] += 1
-            key = tuple(count) # compute once
+            
+            key = tuple(count)   # compute once
+            
             if key in out:
                 out[key].append(st)
             else:
                 out[key] = [st]
+        
         return list(out.values())
