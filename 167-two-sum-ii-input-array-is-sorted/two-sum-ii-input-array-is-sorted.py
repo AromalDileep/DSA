ce002f7b15023f4f0dbecb3a0eb2a9ac7cf3fbc0
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-04-06 16:08:35 +0530

    Time: 7 ms (29.12%) | Memory: 20.4 MB (84.20%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,20 +1,10 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        def binary_search(left, right, target_val):
-            while left <= right:
-                mid = (left + right) // 2
-                if numbers[mid] == target_val:
-                    return mid
-                elif numbers[mid] < target_val:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return -1
-        
         for i in range(len(numbers)):
             complement = target - numbers[i]
             
-            j = binary_search(i + 1, len(numbers) - 1, complement)
-            if j != -1:
-                return [i + 1, j + 1]
+            j = bisect.bisect_left(numbers, complement, i+1)
+            
+            if j < len(numbers) and numbers[j]  == complement:
+                return [i+1, j+1]
