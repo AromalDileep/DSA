a78b240e80ec1946e500148c1b06d1b4231acea0
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-04-12 11:20:32 +0530

    Time: 239 ms (33.90%) | Memory: 30.5 MB (45.20%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.py
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.py
@@ -1,24 +1,32 @@
 class Solution:
     def minimumDistance(self, word: str) -> int:
 
-        @cache
+        memo = {}
+
         def dp(si, f1, f2):
 
             if si == len(word):
                 return 0
-            
+
+            # check cache
+            if (si, f1, f2) in memo:
+                return memo[(si, f1, f2)]
+
             c = word[si]
             pos = ord(c) - ord("A")
-            i, j = pos//6, pos%6
+            i, j = pos // 6, pos % 6
 
-            best = float("inf")
+            distance1 = 0 if f1 == (-1, -1) else abs(f1[0]-i) + abs(f1[1]-j)
+            cost1 = distance1 + dp(si+1, (i, j), f2)
 
-            distance1 = 0 if f1 == (-1,-1) else abs(f1[0]-i) + abs(f1[1]-j)
-            best = min(best, distance1 + dp( si+1, (i, j), f2))
-    
-            distance2 = 0 if f2 == (-1,-1) else abs(f2[0]-i) + abs(f2[1]-j)
-            best = min(best, distance2 + dp( si+1, f1, (i,j)))
+            distance2 = 0 if f2 == (-1, -1) else abs(f2[0]-i) + abs(f2[1]-j)
+            cost2 = distance2 + dp(si+1, f1, (i, j))
+
+            best = min(cost1, cost2)
+
+            # store result
+            memo[(si, f1, f2)] = best
 
             return best
-        
-        return dp(0, (-1,-1),(-1,-1))
+
+        return dp(0, (-1, -1), (-1, -1))
