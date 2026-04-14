965af2938faac519fbb59c824370f855937a3026
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-04-14 15:25:20 +0530

    Time: 3 ms (42.02%) | Memory: 19.3 MB (47.37%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        
+        if len(s) <= k:
+            return s[::-1]
+        s = list(s)
+        
+        for i in range(0, len(s), 2*k):
+            l, r = i,  min(i + k-1, len(s)-1)
+            while l < r:
+                s[l], s[r] = s[r], s[l]
+                l += 1 
+                r -= 1
+        
+        return "".join(s)
