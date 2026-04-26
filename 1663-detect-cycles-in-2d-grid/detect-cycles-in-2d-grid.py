b6e77ab97b051a18e11a45d16d38832514132cff
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-04-26 13:09:05 +0530

    Time: 271 ms (89.83%) | Memory: 85.3 MB (67.67%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -1,39 +1,36 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
+        
         rows, cols = len(grid), len(grid[0])
+        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        visited = [[False] * cols for _ in range(rows)]
 
-        visited = [[False]* cols for _ in range(rows)]
-
-        dirs = [(1,0), (-1,0), (0,1),(0,-1)]
-
-        def dfs(r,c, pr, pc, char):
-            visited[r][c] = True 
+        def dfs(r, c, pr, pc, char):
+            visited[r][c] = True
 
             for dr, dc in dirs:
                 nr = r + dr
-                nc = c + dc 
-            
+                nc = c + dc
+
                 if nr < 0 or nc < 0 or nr >= rows or nc >= cols:
                     continue
-
+                
                 if grid[nr][nc] != char:
                     continue
-
+                
                 if not visited[nr][nc]:
-                    if dfs(nr,nc, r, c, char):
+                    if dfs(nr, nc, r, c, char):
                         return True
                 
-                # if visited and not parent (only reach this line if visited)
                 elif nr != pr or nc != pc:
                     return True
             
             return False
-            
         
-        for r in range(rows):
+        for  r in range(rows):
             for c in range(cols):
                 if not visited[r][c]:
-                    if dfs(r,c, -1,-1, grid[r][c]):
+                    if dfs(r,c,-1,-1,grid[r][c]):
                         return True
         
         return False
