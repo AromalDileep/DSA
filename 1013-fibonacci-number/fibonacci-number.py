d954f7fba337697c292f74df0ebeeb9bfb643453
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-05-08 10:51:27 +0530

    Time: 48 ms (55.68%) | Memory: 19.3 MB (19.32%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        sqrt5 = 5 ** 0.5
-        phi = (1 + sqrt5) / 2 
-        psi = (1 - sqrt5) / 2
+        phi = (1 + 5 ** 0.5 ) / 2
+        psy = (1 - 5 ** 0.5) / 2 
 
-        fib_n = (phi**n - psi**n) / sqrt5 
-        return int(fib_n)
+        return int((phi**n - psy**n)//5**0.5)
+    
