ac66b18071e6537cda957c568484d6cf7810d68a
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-05-08 10:53:45 +0530

    Time: 41 ms (85.27%) | Memory: 19.4 MB (19.32%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
         phi = (1 + 5 ** 0.5 ) / 2
-        psy = (1 - 5 ** 0.5) / 2 
+        psi = (1 - 5 ** 0.5) / 2 
 
-        return round((phi**n - psy**n)//5**0.5)
+        return round((phi**n - psi**n)//5**0.5)
     
