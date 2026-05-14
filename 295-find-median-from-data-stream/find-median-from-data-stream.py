97003f341f4b55a51d590592ce5a906f758997d2
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-05-14 13:16:59 +0530

    Time: 257 ms (15.54%) | Memory: 42.4 MB (30.89%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,23 +1,15 @@
 class MedianFinder:
 
     def __init__(self):
-        #  (Max Heap)
-        self.small = []  
-        # (Min Heap)
-        self.large = []  
+        self.list = SortedList()
 
     def addNum(self, num: int) -> None:
-        # 1. Push to small (max heap)
-        heapq.heappush_max(self.small, num)
-
-        # 2. Balance: move largest of small to large (min heap)
-        heapq.heappush(self.large, heapq.heappop_max(self.small))
-
-        # 3. Maintain size: small can be equal or 1 greater than large
-        if len(self.large) > len(self.small):
-            heapq.heappush_max(self.small, heapq.heappop(self.large))
+        self.list.add(num)
 
     def findMedian(self) -> float:
-        if len(self.small) > len(self.large):
-            return float(self.small[0])
-        return (self.small[0] + self.large[0]) / 2
+        n = len(self.list)
+        mid = n // 2 
+        if n % 2 == 0:
+            return (self.list[mid] + self.list[mid-1]) / 2
+        
+        return self.list[mid]
