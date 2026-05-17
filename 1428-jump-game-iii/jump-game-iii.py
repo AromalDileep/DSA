b65eee7914ceb453497dd1d6b19c74f85a9b878d
Aromal <154106565+AromalDileep@users.noreply.github.com>
2026-05-17 08:35:41 +0530

    Time: 11 ms (65.93%) | Memory: 37.2 MB (28.37%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -1,17 +1,16 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         n = len(arr)
-        visited = set()
 
         def dfs(i):
-            if i < 0 or i >=n or i in visited:
+            if i < 0 or i >=n or arr[i] < 0:
                 return False
             
 
             if arr[i] == 0:
                 return True
 
-            visited.add(i)
+            arr[i] = -arr[i]
 
             return dfs(i + arr[i]) or dfs(i - arr[i])
 
